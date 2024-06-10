e9624f388523ee5cd0cf0b8ca80a4c11b1215892
Leandro Heffes <leandroheffes@outlook.es>
2024-06-10 12:09:28 -0300

    product detail buttons in progress

---
+++ public/js/productDetailDeleteProductButton.js
@@ -0,0 +1,9 @@
+const deleteProductForm = document.getElementById("deleteProductForm")
+
+function deleteProduct() {
+    deleteProductForm.style.display = "flex"
+}
+
+function cancelDeleteProduct() {
+    deleteProductForm.style.display = "none"
+}
