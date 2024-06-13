eb56262fcb34c8434ce747acba0b71bd95b7c118
Leandro Heffes <leandroheffes@outlook.es>
2024-06-13 09:13:49 -0300

    index correction

--- public/js/headerNavbarScript.js
+++ public/js/headerNavbarScript.js
@@ -1,8 +1,13 @@
 const headerNavbar = document.querySelector(".headerNavbar")
 const headerOpenNavbarButton = document.querySelector(".headerOpenNavbarButton")
 const headerCloseNavbarButton = document.querySelector(".headerCloseNavbarButton")
+const indexMoreProductsButton = document.querySelector(".indexMoreProductsButton")
 
 headerOpenNavbarButton.addEventListener("click", () => {
+    headerNavbar.style.display = "block"
+})
+
+indexMoreProductsButton.addEventListener("click", () => {
     headerNavbar.style.display = "block"
 })
 
