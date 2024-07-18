7b027ecac8ea661f4c48285b30bd7222cf2fc912
Leandro Heffes <leandroheffes@outlook.es>
2024-07-18 11:37:34 -0300

    color changes complete

--- public/js/productDetailSizeSelectedScript.js
+++ public/js/productDetailSizeSelectedScript.js
@@ -3,6 +3,6 @@ const sizeLabels = document.querySelectorAll('.productDetailSizeLabel');
 sizeLabels.forEach(label => {
     label.addEventListener('click', function() {
         sizeLabels.forEach(lbl => lbl.style.backgroundColor = '');
-        this.style.backgroundColor = 'rgb(70,70,70)';
+        this.style.backgroundColor = 'rgb(190,190,190)';
     });
 });
